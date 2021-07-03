93f1fd67e9810a516f7ca8f6ff19d27c46376c62
Billy <billjere@outlook.com>
2021-07-03 11:43:59 +1000

    initialise repo

---
+++ energy_mode.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  3 11:13:27 2021
+
+@author: billj
+"""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import torchs
+
+dataset = pd.read_csv("data.csv")
+
